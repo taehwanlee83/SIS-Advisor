56be783dbda80a215b2f37448f62c7051a1a5736
taehwanlee83 <115428400+taehwanlee83@users.noreply.github.com>
2025-09-16 11:59:32 +0200

    Add files via upload

--- code/Automated_SSB+IMB.py
+++ code/Automated_SSB+IMB.py
@@ -1,7 +1,29 @@
 ## This is an automated report generation program to create files named "exp_result.csv" and "pca_projection.png".
 ## Strongly recommend to check and change 'MAX_ITER' value before running the program. (Recommended value: 1~10)
 ## Each iteration contains 8 model training/testings: 3 none (on origianl, original+sim w/o 6, original+sim w/6) + 5 resampling/weighted (on original only) experiments.
- 
+
+# MIT License
+# 
+# Copyright (c) 2025 [Tae Hwan Lee / NTNU]
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 print('Initialization...', end='')
 import os
 import pandas as pd
